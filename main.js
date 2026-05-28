9a9b39c26ac5a3eb7db4f4db280036230887f68d
natszsui-oss <mateusbuss2015@gmail.com>
2026-05-28 15:21:28 +0000

    aula 6 0.5

--- main.js
+++ main.js
@@ -35,4 +35,22 @@ function calculaTempo(tempoObjetivo) {
     " segundos"
   );
 }
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempos[0]);
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
